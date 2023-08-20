455e3aff6608c9629d36799438f7aea076adf8d4
Jorge <Jorge@DESKTOP-5DL7PLT>
2023-08-19 18:50:13 -0600

    Agregar archivos de proyecto.

---
+++ Entry_3010746/Entry_3010746/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Entry_3010746
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Entry_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string pasado = e.OldTextValue;
+            string nuevo = e.NewTextValue;
+        }
+
+        private void Entry_Completed(object sender, EventArgs e)
+        {
+            string textO = ((Entry)sender).Text;
+
+        }
+    }
+}
